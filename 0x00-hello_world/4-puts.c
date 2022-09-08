5bdfa4bb9d0cca46954a47125abb94a828562007
Usman Sharafa <usmansharafa04@gmail.com>
2022-09-08 05:03:13 -0700

    Task 4 solved

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Print out '\"Programming is like building a multilingual puzzle'
+ *
+ * Return: 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
