cfa2b2951890f3afd4039cace1afc8389f078142
Usman Sharafa <usmansharafa04@gmail.com>
2022-09-09 15:00:57 -0700

    Task 5 solved

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,14 +5,16 @@
  *
  * Description: Prints all numbers of base 10
  *
- * Return: 0 (Success)
+ * Return: 0 (Always Success)
  */
 int main(void)
 {
-    for (int i = 0; i < 10; i++)
-    {
-        printf("%i", i);
-    }
-    putchar('\n');
-    return (0);
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	putchar('\n');
+	return (0);
 }
