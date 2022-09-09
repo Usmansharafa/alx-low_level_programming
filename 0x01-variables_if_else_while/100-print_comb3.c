e36d7650b5e4e65fe781da133c88d8305b985fdd
Usman Sharafa <usmansharafa04@gmail.com>
2022-09-09 15:36:08 -0700

    Task 10 solved

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints possible combination of two different digits
+ *
+ * Return: 0 (Always Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i < 57; i++)
+	{
+		for (j = i + 1; j < 58; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i == 56 && j == 57)
+			{
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
+			continue;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
