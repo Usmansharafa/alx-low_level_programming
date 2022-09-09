8f66fb477a7a33f80d7ebdfa45719bb2a159e8ea
Usman Sharafa <usmansharafa04@gmail.com>
2022-09-09 15:04:13 -0700

    Task 6 solved

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,14 +5,16 @@
  *
  * Description: Prints all digits of base 10
  *
- * Return: 0 (Success)
+ * Return: 0 (Always Success)
  */
 int main(void)
 {
-    for (int i = 48; i < 58; i++)
-    {
-        putchar(i);
-    }
-    putchar('\n');
-    return (0);
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }
