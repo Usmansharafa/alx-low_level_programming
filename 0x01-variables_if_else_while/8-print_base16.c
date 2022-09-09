46b2a350be4117d495601225730a015b2d01845d
Usman Sharafa <usmansharafa04@gmail.com>
2022-09-09 15:15:46 -0700

    Task 8 solved

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,18 +5,20 @@
  *
  * Description: Prints all numbers of base 16 in lowercase
  *
- * Return: 0 (Success)
+ * Return: 0 (Always Success)
  */
 int main(void)
 {
-    for (int i = 48; i < 58; i++)
-    {
-        putchar(i);
-    }
-    for (int i = 97; i < 103; i++)
-    {
-        putchar(i);
-    }
-    putchar('\n');
-    return (0);
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }
