241e92d74fdb42fc3c4e94e3c85139cce971d27b
Usman Sharafa <usmansharafa04@gmail.com>
2022-09-09 14:55:40 -0700

    Task 4 solved

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,18 +5,20 @@
  *
  * Description: Prints alphabet in lowercase excluding q and e
  *
- * Return: 0 (Success)
+ * Return: 0 (Always Success)
  */
 int main(void)
 {
-    for (int i = 97; i < 123; i++)
-    {
-        if (i == 101 || i == 113)
-        {
-            continue;
-        }
-        putchar(i);
-    }
-    putchar('\n');
-    return (0);
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		if (i == 101 || i == 113)
+		{
+			continue;
+		}
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }
