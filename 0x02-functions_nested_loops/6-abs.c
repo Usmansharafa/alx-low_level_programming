89bca35f7a5697f5beb426df0eefcc49f8313cbc
Usman Sharafa <usmansharafa04@gmail.com>
2022-09-13 03:12:07 -0700

    Task 6 solved

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - returns the absolute value of a number
+ *
+ * @n: number to be checked
+ *
+ * Return: absolute value of param n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	return (n);
+}
