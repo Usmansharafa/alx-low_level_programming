f94a89c4cba9dcb3288244bc9218c96363d4466d
Usman Sharafa <usmansharafa04@gmail.com>
2022-09-13 07:47:14 -0700

    Task 14 solved

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the first 50 Fibonacci numbers starting with 1 and 2
+ *
+ * Return: 0 (Always Success)
+ */
+int main(void)
+{
+	int count;
+	unsigned long prev, next, sum;
+
+	prev = 1;
+	next = 2;
+	sum = 3;
+
+	for (count = 0; count < 50; count++)
+	{
+		if (count == 0)
+		{
+			printf("%lu, ", prev);
+		}
+		else if (count == 1)
+		{
+			printf("%lu, ", next);
+		}
+		else
+		{
+			printf("%lu, ", sum);
+			prev = next;
+			next = sum;
+			sum = sum + prev;
+		}
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
