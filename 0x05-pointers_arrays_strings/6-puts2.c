9930e033b1aa24b33d4d71097950ccaa9cda0958
Usman Sharafa <usmansharafa04@gmail.com>
2022-09-19 05:36:21 -0700

    Task 6 solved

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+int _strlen(char *s);
+/**
+ * puts2 - prints even positioned characters in a string
+ *
+ * @str: string to be worked upon
+ */
+void puts2(char *str)
+{
+	int len, i;
+
+	len = _strlen(str);
+	for (i = 0; i < len; i + 2)
+		_putchar(str[i]);
+}
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: string whose length is to be found
+ *
+ * Return: Length of string s
+ */
+int _strlen(char *s)
+{
+	int count;
+
+	for (count = 0; s[count] != 0; ++count)
+		;
+
+	return (count);
+}
