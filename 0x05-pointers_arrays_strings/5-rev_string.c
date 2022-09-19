58f2b4bbcaae96a72e2b6e5579ddf6f1b29a1cf9
Usman Sharafa <usmansharafa04@gmail.com>
2022-09-19 05:19:05 -0700

    Task 5 solved

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+void swap(char *a, char *b);
+
+/**
+ * rev_string - Reverses a string
+ *
+ * @s: string to be reversed
+ */
+void rev_string(char *s)
+{
+	int i, len, half;
+	char temp;
+
+	len = _strlen(s);
+	half = len / 2;
+
+	for (i = 0; i < half; ++i)
+	{
+		temp = s[i];
+		s[i] = s[len - i + 1];
+		s[len - i + 1] = temp;
+	}
+
+}
+
+/**
+ * _strlen - gets the length of a string
+ *
+ * @s: string whose length is to be found
+ *
+ * Return: Length of string
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	for (length = 0; s[length] != '\0'; ++length)
+		;
+	return (length);
+}
