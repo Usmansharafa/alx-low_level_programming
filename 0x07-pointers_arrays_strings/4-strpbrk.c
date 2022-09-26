9cf67e4513554e30a6bba621e8fde5f362b5bc1e
Usman Sharafa <usmansharafa04@gmail.com>
2022-09-26 06:13:33 -0700

    Task 4 solved

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Locates the first occurence in the string s of
+ * any of the bytes in string accept
+ *
+ * @s: string to be checked
+ *
+ * @accept: any byte of string to look for in s
+ *
+ * Return: Pointer to the byte in s that matches one of the bytes
+ * in s or NULL if no such byte is found
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j, count;
+
+	i = 0;
+	count = 0;
+	while (s[i] != '\0')
+	{
+		for (j = 0; accept[j] != '\0'; ++j)
+		{
+			if (s[i] == accept[j])
+				count++;
+		}
+		if (count > 0)
+			break;
+		i++;
+	}
+	if (count > 0)
+		return (s + i);
+	return (NULL);
+}
