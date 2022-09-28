03fbdaa7bd12547d225aa02d47e8215e90a9cec4
Usman Sharafa <usmansharafa04@gmail.com>
2022-09-28 04:35:02 -0700

    Task 0 solved

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string followed by a newline
+ *
+ * @s: string to be printed
+ */
+
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	if (*(s + 1) != '\0')
+		_puts_recursion(s + 1);
+	_putchar('\n');
+}
