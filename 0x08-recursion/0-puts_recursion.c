0d6eab74be5e70fe24ad4ecdb92e17935da08f79
Usman Sharafa <usmansharafa04@gmail.com>
2022-09-28 04:48:06 -0700

    Task 0 solved

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,8 +9,8 @@
 void _puts_recursion(char *s)
 {
 	_putchar(*s);
-	if (*(s + 1) != '\0')
-		_puts_recursion(s + 1);
 	if (*s == '\0')
 		_putchar('\n');
+	if (*(s + 1) != '\0')
+		_puts_recursion(s + 1);
 }
