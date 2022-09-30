4722761401794ea69d30d1f53615bddc6d2f9ec9
Usman Sharafa <usmansharafa04@gmail.com>
2022-09-30 03:46:32 -0700

    Task 3 solved

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Multiplies two numbers
+ * @argc: Argument count
+ * @argv: Argument value
+ * Return: 1 if no argument is passed or 0 if successful
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+
+	if (argc == 1 || argc > 3)
+	{
+		printf("Ërror\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	printf("%i\n", num1 * num2);
+
+	return (0);
+}
