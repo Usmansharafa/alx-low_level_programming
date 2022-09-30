d98ca38cdb065711989230da1afc097eff8b68a8
Usman Sharafa <usmansharafa04@gmail.com>
2022-09-30 07:03:52 -0700

    Added check to confirm validity of arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 	}
 
 	if ((argv[1] != '0' && atoi(argv[1]) == 0) ||
-	(argv[2] != '0' && atoi(argv[2] == 0))
+	(argv[2] != '0' && atoi(argv[2]) == 0)
 	{
 		printf("Error\n");
 		return (1);
