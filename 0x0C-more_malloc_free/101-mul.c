89d73d0007fa70a925fa0a585df91472df254692
Usman Sharafa <usmansharafa04@gmail.com>
2022-10-06 10:24:40 -0700

    Task 5 solved

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: Multiplies two cmdline arguments
+ * @argc: Number of cmdline arguments
+ * @argv: argument vector
+ * Return: Product of two cmdline arguments
+ */
+int main(int argc, char *argv[])
+{
+	unsigned long long int num1, num2, mul;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if ((atoi(argv[1]) == 0 && argv[1] != "0") ||
+	(atoi(argv[2]) == 0 && argv[2] != "0"))
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	mul = num1 * num2;
+
+	printf("%llu\n", mul);
+
+	return (0);
+}
