9e778290ac03b377686554a9d8028ca2ec7a8a39
Usman Sharafa <usmansharafa04@gmail.com>
2022-10-06 10:02:12 -0700

    Task 1 solved

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -23,6 +23,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	total = s1_len + n;
 
 	new = malloc(sizeof(char) * total + 1);
+	if (new == NULL)
+		return (NULL);
 	for (i = 0; i < s1_len; i++)
 		new[i] = s1[i];
 	for (i = 0; i < n; i++)
