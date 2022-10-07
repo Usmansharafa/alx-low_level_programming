c695cfd1336166119ab85612918ca57a4e22e5a1
Usman Sharafa <usmansharafa04@gmail.com>
2022-10-07 09:11:59 -0700

    Task 5 solved

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -15,8 +15,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if ((atoi(argv[1]) == 0 && argv[1] != "0") ||
-	(atoi(argv[2]) == 0 && argv[2] != "0"))
+	if (check(argv[1]) == 1 || check(argv[2]) == 1)
 	{
 		printf("Error\n");
 		exit(98);
@@ -25,7 +24,26 @@ int main(int argc, char *argv[])
 	num2 = atoi(argv[2]);
 	mul = num1 * num2;
 
-	printf("%llu\n", mul);
+	printf("%u\n", mul);
+
+	return (0);
+}
+
+/**
+ * check - Checks whether a string can be converted to
+ * a valid number
+ * @str: string to be checked
+ * Return: 0 if it can or 1 if not
+ */
+int check(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != 0; i++)
+	{
+		if (str[i] < 48 || str[i] > 57)
+			return (1);
+	}
 
 	return (0);
 }
