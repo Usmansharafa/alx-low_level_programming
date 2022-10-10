531ba965e7b87aa04ff54951741c1ad25184618c
Usman Sharafa <usmansharafa04@gmail.com>
2022-10-10 05:52:44 -0700

    All tasks solved

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ * Description: Prints out the name of the file
+ * Return: nothing to be returned
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
