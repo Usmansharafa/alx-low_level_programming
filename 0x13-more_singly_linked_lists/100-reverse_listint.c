2ef1f362fda22c1124475295245f4608fc3910b8
Usman Sharafa <usmansharafa04@gmail.com>
2022-10-25 16:23:18 -0700

    Task 11 solved

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,5 +1,10 @@
 #include "lists.h"
 
+/**
+ * reverse_listint - Reverses a linked list
+ * @head: Pointer to address of linked list
+ * Return: Pointer to head node of reversed list
+ */
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *prev = NULL;
