8ddfff438dec51f7e4a640068f996866c6de2d37
Usman Sharafa <Usmansharafa04@gmail.com>
2022-12-08 14:51:19 +0100

    Task 5 solved

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -9,18 +9,16 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int count = 0;
-	dlistint_t *current;
 
 	if (!head)
 		return (NULL);
 	while (head->prev != NULL)
 		head = head->prev;
-	current = head;
-	while (current->next)
+	while (head)
 	{
 		if (count == index)
-			return (current);
-		current = current->next;
+			return (head);
+		head = head->next;
 		count++;
 	}
 	return (NULL);
